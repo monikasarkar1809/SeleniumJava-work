cdf119409ce2099e124e73cae0b96de386ea8355
Monika Sarkar <monikasarkar1809@gmail.com>
2025-08-02 16:28:36 +0530

    Update Webdriver.java

--- src/project/Webdriver.java
+++ src/project/Webdriver.java
@@ -1,5 +1,37 @@
-package project;
+package myclassdemo;
 
-public class Webdriver {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class myclassdemo {
+
+	public static <webElement> void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Driver File\\All driver folder\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		
+		
+		
+		driver.get("https://www.saucedemo.com/");
+		
+		//locate username by id
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		
+		//locate password by name
+		driver.findElement(By.name("password")).sendKeys("secret_sauce");
+		
+		//locate login button by class
+		driver.findElement(By.className("btn_action")).click();
+		
+		//Switch to product
+		String currWindowHandle = driver.getWindowHandle();
+		driver.switchTo().window(currWindowHandle);
+
+		WebElement elementlist = driver.findElement(By.linkText("Sauce"));
+		System.out.println("Element size:"+  elementlist.getSize());
+		
+
+	}
 
 }
